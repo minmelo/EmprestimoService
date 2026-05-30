437006320fe66d429faf646713d508d5a73914c1
yasmin melo <maigbm@icloud.com>
2026-05-30 03:57:56 -0300

    inicial

---
+++ src/main/java/br/com/banco/emprestimoservice/EmprestimoserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.banco.emprestimoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoserviceApplication.class, args);
+	}
+
+}
